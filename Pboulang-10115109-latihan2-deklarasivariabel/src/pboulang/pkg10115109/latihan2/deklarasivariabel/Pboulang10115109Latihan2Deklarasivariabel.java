a23b4282887668055943ba32d8a5c3866a25b184
IndriYuliyanti <55839650+IndriYuliyanti@users.noreply.github.com>
2019-09-26 21:08:28 +0700

    Add files via upload

---
+++ Pboulang-10115109-latihan2-deklarasivariabel/src/pboulang/pkg10115109/latihan2/deklarasivariabel/Pboulang10115109Latihan2Deklarasivariabel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10115109.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author User
+ */
+public class Pboulang10115109Latihan2Deklarasivariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
